4225f429cdc03ac9bbf8944b51bc7a08e969312d
blep <tagendiar+github@outlook.com>
2022-01-31 23:22:17 +0100

    Add info field for ExpressionUIs

--- Editor/ExpressionUI.cs
+++ Editor/ExpressionUI.cs
@@ -8,15 +8,18 @@ namespace ExpressionUtility.UI
 {
 	internal class ExpressionUI : ScriptableObject
 	{
-		[SerializeField]
+		[SerializeField, Header("The name that this Expression will be shown as")]
 		private string _name;
-		[SerializeField, TextArea]
+		[SerializeField, TextArea, Header("The description of the Expression in the UI")]
 		private string _description;
-		[SerializeField]
-		private VisualTreeAsset _layout;
-		[SerializeField]
+		[SerializeField, Header("The icon that will be shown for this Expression")]
 		private Texture2D _icon;
 		
+		[SerializeField, TextArea, Header("Use this to display usage information in the bottom of the UI when in use")]
+		private string _infoBox;
+		[SerializeField, Header("The UI layout file to use")]
+		private VisualTreeAsset _layout;
+
 		public VisualTreeAsset Layout => _layout;
 		public Texture2D Icon => _icon;
 

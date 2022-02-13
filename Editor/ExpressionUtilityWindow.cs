bcc160b7be72f2738ed84f5ef53edd6f09ee855d
blep <tagendiar+github@outlook.com>
2022-02-13 22:16:48 +0100

    Add a workaround for Unity messing up our ScriptableObjects after package updates

--- Editor/ExpressionUtilityWindow.cs
+++ Editor/ExpressionUtilityWindow.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Globalization;
 using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEditor.SceneManagement;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 namespace ExpressionUtility.UI
 {
@@ -17,11 +21,37 @@ namespace ExpressionUtility.UI
 
 		private void OnEnable()
 		{
-			this.SetAntiAliasing(4);
-			titleContent = EditorGUIUtility.TrTextContentWithIcon("Expression Utility", "NetworkAnimator Icon");
-			_controller = new UIController(this, _assets);
-			_controller.SetFrame<Intro>();
-			EditorSceneManager.sceneOpened += SceneChanged;
+			try
+			{
+				this.SetAntiAliasing(4);
+				titleContent = EditorGUIUtility.TrTextContentWithIcon("Expression Utility", "NetworkAnimator Icon");
+				_controller = new UIController(this, _assets);
+				_controller.SetFrame<Intro>();
+				EditorSceneManager.sceneOpened += SceneChanged;
+			}
+			catch (NullReferenceException e)
+			{
+				// Unity will sometimes drop UXML references that have been assigned in the inspector when moving/installing packages..
+				// A script reload will fix this, so let's do that as a workaround for now ;_; 
+				// If we have an actual legit null reference we don't want to get stuck in an endless script reload-loop, so we keep track of last time we did this
+				
+				const string RELOAD_TIMESTAMP_KEY = "expression-utility-reload-timestamp";
+				const int MIN_RELOAD_WAIT_TIME = 10;
+				
+				var now = DateTime.UtcNow;
+				var nowString = now.ToString("O");
+				var lastString = EditorPrefs.GetString(RELOAD_TIMESTAMP_KEY, DateTime.MinValue.ToString("O"));
+				DateTime last = DateTime.Parse(lastString, CultureInfo.InvariantCulture);
+				
+				EditorPrefs.SetString(RELOAD_TIMESTAMP_KEY, nowString);
+				if (now - last > TimeSpan.FromSeconds(MIN_RELOAD_WAIT_TIME))
+				{
+					CompilationPipeline.RequestScriptCompilation();
+					var text = new Label("Please wait...");
+					text.AddToClassList("header--center");
+					rootVisualElement.Add(text);
+				}
+			}
 		}
 
 		private void SceneChanged(Scene _, OpenSceneMode __)
